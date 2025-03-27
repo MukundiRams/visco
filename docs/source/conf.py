cc715d53a69fee575d8ba6f3a7fb281f8a367ff4
MukundiRams <2094318@students.wits.ac.za>
2025-03-27 12:32:35 +0000

    fixed the RTD config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,22 +15,39 @@ copyright = '2025, Mukundi Ramanyimi'
 author = 'Mukundi Ramanyimi'
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',  # Automatically generate documentation from docstrings
-    'sphinx.ext.viewcode',  # Add links to source code
-    'sphinx_autodoc_typehints',  # Document type hints
-]
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests']
+exclude_patterns = []
 
 language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+html_static_path = ['_static']
 
+
+
+needs_sphinx = "5.0"
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks",
+]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
