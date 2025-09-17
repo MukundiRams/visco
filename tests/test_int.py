8cbb0da9f63fbfb014c1253b3057b2c17c07fe09
MukundiRams <2094318@students.wits.ac.za>
2025-09-17 07:29:09 +0000

    modified the tests in line with new changes

--- tests/test_int.py
+++ tests/test_int.py
@@ -1,6 +1,6 @@
 import unittest
-from visco.archive import archive_visdata
-from visco.decompress import decompress_visdata
+from visco.compress_ms import compress_full_ms
+from visco.decompress_ms import write_datasets_to_ms
 import os
 
 
@@ -14,29 +14,35 @@ class TestArchiveFunction(unittest.TestCase):
         """Test the full archival and decompression pipeline."""
         
         # Step 1: Compress the data
-        archive_visdata(
-            ms=self.test_ms,
+        compress_full_ms(
+            ms_path=self.test_ms,
+            zarr_path=f"{self.compressed_file}",
+            consolidated=True,
+            chunk_size_row=5000,
+            overwrite=True,
+            compressor='zstd',
+            level=4,
+            nworkers=1,
+            nthreads=1,
+            memory_limit="2GB",
+            direct_to_workers=False,
             fieldid=0,
             ddid=0,
             scan=1,
             correlation = 'XX,YY',
+            correlation_optimized=False,
             column='DATA',
+            outcolumn='COMPRESSED_DATA',
             compressionrank=1,
-            outfilename=self.compressed_file,
-            autocorrelation=False,
-            weightcr=2,
-            flagvalue=0.5 + 0.02j,
-            antlist=[0,1,2],
-            decorrelation=None
             
         )
-        self.assertTrue(os.path.exists(f"zarr-output/{self.compressed_file}"))
+        self.assertTrue(os.path.exists(f"{self.compressed_file}"))
 
         # Step 2: Decompress the data
-        decompress_visdata(
-            zarr_path=f"zarr-output/{self.compressed_file}",
-            output_column='DATA',
-            output_ms="decompressed.ms"
+        write_datasets_to_ms(
+            zarr_path=f"{self.compressed_file}",
+            column='COMPRESSED_DATA',
+            msname="decompressed.ms"
             
         )
         self.assertTrue(os.path.exists("decompressed.ms"))
